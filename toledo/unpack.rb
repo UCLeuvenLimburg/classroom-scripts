f01d3580513c3b2e1487b6da7a646ef598705a6a
Frederic Vogels <frederic.vogels@gmail.com>
2018-06-14 11:48:40 +0200

    toledo/unpack.rb

--- toledo/unpack.rb
+++ toledo/unpack.rb
@@ -45,11 +45,17 @@ Zip::File.open(ARGV[0]) do |zip|
   zip.each do |entry|
     /_(q\d{7})_/ =~ entry.name or abort "Could not find q-id in #{entry.name}"
     qid = $1
+    slug = qtable[qid]
 
-    puts "Creating directory #{qid} (if it does not exist yet)"
-    FileUtils.mkdir_p qid
+    if not slug
+      puts "Unrecognized q-id #{qid}"
+      abort
+    end
 
-    puts "Extracting #{entry.name} to #{qid}"
-    entry.extract("#{qid}/#{entry.name}")
+    puts "Creating directory #{slug} (if it does not exist yet)"
+    FileUtils.mkdir_p slug
+
+    puts "Extracting #{entry.name} to #{slug}/#{entry.name}"
+    entry.extract("#{slug}/#{entry.name}")
   end
 end
