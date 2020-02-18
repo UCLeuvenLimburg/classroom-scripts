9d15429b5f26abc307c8e13e7cd32380d7ae4d57
Frederic Vogels <frederic.vogels@ucll.be>
2020-02-18 09:27:08 +0100

    generate-clone-script cleanup

--- classroom/generate-clone-script.rb
+++ classroom/generate-clone-script.rb
@@ -2,21 +2,21 @@ require 'rubygems'
 require 'bundler/setup'
 require 'octokit'
 
-REPO = 'pvm1718'
-
+REPO = 'ucll-vgo1920-sem1'
+PROJECT = 'picross'
 
 token = IO.read('token.secret').strip
 G = github = Octokit::Client.new(:access_token => token, per_page: 150)
 
 github.organization_repositories(REPO).each do |repo|
     url = repo.clone_url
-    /^exercises-(.*)$/ =~ repo.name or abort "Could not parse #{repo.name}"
+    /^#{PROJECT}-(.*)$/ =~ repo.name or abort "Could not parse #{repo.name}"
     name = $1
-    puts <<-END
-if [ ! -d #{name} ]; then
-    git clone #{url} #{name}
-else
-    (cd #{name}; git pull --no-edit)
-fi
+    puts <<~END
+        if [ ! -d #{name} ]; then
+            git clone #{url} #{name}
+        else
+            (cd #{name}; git pull --no-edit)
+        fi
     END
 end
